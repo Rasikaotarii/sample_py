5a6eda588022e9976ce4090babeec8151a31b6d0
Rasikaotarii <rasikaotari1011@gmail.com>
2026-01-08 10:53:23 +0530

    Add files via upload

---
+++ add twono.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+int = a,real; 
+string = b, imaginary;
+
+void show () {
+
+cout<<"real number"<<endl;
+cout<<"imaginary number"<<endl;
+cout<<"real number "+!" imaginary number"<<endl;
+}
+display(){
+
+printf("add the real number +! imaginary number");
+}
+int main() {
+
+real number = 2
+imaginary number = 5
+return 0;
+}
+
+
+
